ce006039756bd2d68a783d469378c4dc8701245f
Dipti Singh <dipti1010.singh@gmail.com>
2025-06-21 16:18:17 +0530

    updated

--- app.py
+++ app.py
@@ -1,67 +1,121 @@
-import streamlit as st
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse, RedirectResponse
+from uvicorn import run as app_run
+
+from typing import Optional
+
+from us_visa.constants import APP_HOST, APP_PORT
 from us_visa.pipline.prediction_pipeline import USvisaData, USvisaClassifier
 from us_visa.pipline.training_pipeline import TrainPipeline
 
-st.set_page_config(page_title="🛂 US Visa Eligibility Classifier", layout="centered")
+app = FastAPI()
 
-st.title("🛂 US Visa Eligibility Classifier")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Sidebar
-# with st.sidebar:
-#     st.header("Developer Options")
-#     if st.button("🚀 Train Model"):
-#         try:
-#             pipeline = TrainPipeline()
-#             pipeline.run_pipeline()
-#             st.success("✅ Model trained successfully!")
-#         except Exception as e:
-#             st.error(f"Training failed: {e}")
+templates = Jinja2Templates(directory='templates')
 
-st.markdown("### ✍️ Enter Visa Application Details Below")
+origins = ["*"]
 
-with st.form("visa_form"):
-    continent = st.selectbox("🌍 Continent", ["Asia", "Africa", "North America", "Europe", "South America", "Oceania"])
-    education_of_employee = st.selectbox("🎓 Education of Employee", ["High School", "Bachelor's", "Master's", "Doctorate"])
-    has_job_experience = st.selectbox("💼 Has Job Experience?", ["Y", "N"])
-    requires_job_training = st.selectbox("📘 Requires Job Training?", ["Y", "N"])
-    
-    no_of_employees = st.number_input("🏢 Number of Employees", min_value=14500, max_value=40000, value=20000)
-    company_age = st.number_input("🏗️ Age of the Company (in months)", min_value=15, max_value=180, value=60)
-    
-    region_of_employment = st.selectbox("🗺️ Region of Employment", ["West", "Northeast", "South", "Midwest", "Island"])
-    prevailing_wage = st.number_input("💵 Prevailing Wage", min_value=600.0, max_value=70000.0, value=60000.0)
-    
-    unit_of_wage = st.selectbox("📆 Contract Tenure", ["Hour", "Year", "Week", "Month"])
-    full_time_position = st.selectbox("🕒 Full Time Position?", ["Y", "N"])
-    
-    submit = st.form_submit_button("🔍 Predict Visa Status")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-if submit:
+class DataForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.continent: Optional[str] = None
+        self.education_of_employee: Optional[str] = None
+        self.has_job_experience: Optional[str] = None
+        self.requires_job_training: Optional[str] = None
+        self.no_of_employees: Optional[str] = None
+        self.company_age: Optional[str] = None
+        self.region_of_employment: Optional[str] = None
+        self.prevailing_wage: Optional[str] = None
+        self.unit_of_wage: Optional[str] = None
+        self.full_time_position: Optional[str] = None
+        
+
+    async def get_usvisa_data(self):
+        form = await self.request.form()
+        self.continent = form.get("continent")
+        self.education_of_employee = form.get("education_of_employee")
+        self.has_job_experience = form.get("has_job_experience")
+        self.requires_job_training = form.get("requires_job_training")
+        self.no_of_employees = form.get("no_of_employees")
+        self.company_age = form.get("company_age")
+        self.region_of_employment = form.get("region_of_employment")
+        self.prevailing_wage = form.get("prevailing_wage")
+        self.unit_of_wage = form.get("unit_of_wage")
+        self.full_time_position = form.get("full_time_position")
+
+@app.get("/", tags=["authentication"])
+async def index(request: Request):
+
+    return templates.TemplateResponse(
+            "usvisa.html",{"request": request, "context": "Rendering"})
+
+
+@app.get("/train")
+async def trainRouteClient():
     try:
-        # Create USvisaData object
-        data = USvisaData(
-            continent=continent,
-            education_of_employee=education_of_employee,
-            has_job_experience=has_job_experience,
-            requires_job_training=requires_job_training,
-            no_of_employees=no_of_employees,
-            company_age=company_age,
-            region_of_employment=region_of_employment,
-            prevailing_wage=prevailing_wage,
-            unit_of_wage=unit_of_wage,
-            full_time_position=full_time_position,
-        )
+        train_pipeline = TrainPipeline()
 
-        input_df = data.get_usvisa_input_data_frame()
+        train_pipeline.run_pipeline()
 
-        # Predict
-        model = USvisaClassifier()
-        prediction = model.predict(dataframe=input_df)[0]
-
-        if prediction == 1:
-            st.success("✅ Visa Status: Approved")
-        else:
-            st.error("❌ Visa Status: Not Approved")
+        return Response("Training successful !!")
 
     except Exception as e:
-        st.error(f"Something went wrong: {e}")
+        return Response(f"Error Occurred! {e}")
+
+
+@app.post("/")
+async def predictRouteClient(request: Request):
+    try:
+        form = DataForm(request)
+        await form.get_usvisa_data()
+        
+        usvisa_data = USvisaData(
+                                continent= form.continent,
+                                education_of_employee = form.education_of_employee,
+                                has_job_experience = form.has_job_experience,
+                                requires_job_training = form.requires_job_training,
+                                no_of_employees= form.no_of_employees,
+                                company_age= form.company_age,
+                                region_of_employment = form.region_of_employment,
+                                prevailing_wage= form.prevailing_wage,
+                                unit_of_wage= form.unit_of_wage,
+                                full_time_position= form.full_time_position,
+                                )
+        
+        usvisa_df = usvisa_data.get_usvisa_input_data_frame()
+
+        model_predictor = USvisaClassifier()
+
+        value = model_predictor.predict(dataframe=usvisa_df)[0]
+
+        status = None
+        if value == 1:
+            status = "Visa-approved"
+        else:
+            status = "Visa Not-Approved"
+
+        return templates.TemplateResponse(
+            "usvisa.html",
+            {"request": request, "context": status},
+        )
+        
+    except Exception as e:
+        return {"status": False, "error": f"{e}"}
+
+
+if __name__ == "__main__":
+    app_run(app, host=APP_HOST, port=APP_PORT)
